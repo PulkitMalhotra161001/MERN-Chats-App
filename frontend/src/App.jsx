674d70d6955ba6139b957e1d4a73404addfac518
Pulkit Malhotra <pulkit@Pulkits-MacBook-Air.local>
2026-01-01 02:34:45 +0530

    Login and logout feature implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,49 +8,54 @@ import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+	const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
+	useEffect(() => {
+		checkAuth();
+	}, [checkAuth]);
 
-  console.log({ authUser });
+	console.log({ authUser });
 
-  // checking and no auth user yet
-  if (isCheckingAuth && !authUser) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
-      </div>
-    );
-  }
+	// checking and no auth user yet
+	if (isCheckingAuth && !authUser) {
+		return (
+			<div className="flex items-center justify-center h-screen">
+				<Loader className="size-10 animate-spin" />
+			</div>
+		);
+	}
 
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route
-          path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-      </Routes>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={authUser ? <HomePage /> : <Navigate to="/login" />}
+				/>
+				<Route
+					path="/signup"
+					element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/login"
+					element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+				/>
+				<Route path="/settings" element={<SettingsPage />} />
+				<Route
+					path="/profile"
+					element={
+						authUser ? <ProfilePage /> : <Navigate to="/login" />
+					}
+				/>
+			</Routes>
+
+			<Toaster />
+		</div>
+	);
 };
 
 export default App;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -10,6 +10,8 @@ import {
 	User,
 } from "lucide-react";
 import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
+import toast from "react-hot-toast";
 
 const SignUpPage = () => {
 	const [showPassword, setShowPassword] = useState(false);
@@ -21,10 +23,27 @@ const SignUpPage = () => {
 
 	const { signup, isSigningUp } = useAuthStore();
 
-	const validateForm = () => {};
+	const validateForm = () => {
+		if (!formData.fullName.trim())
+			return toast.error("Full Name is required");
+		if (!formData.email.trim()) return toast.error("Email is required");
+		if (!/\S+@\S+\.\S+/.test(formData.email))
+			return toast.error("Invalid email format");
+		if (!formData.password) return toast.error("Password is required");
+		if (!formData.password.length < 6)
+			return toast.error("Password must be at least 6 characters");
+
+		return true;
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+
+		const success = validateForm();
+
+		if (success) {
+			signup(formData);
+		}
 	};
 
 	return (
@@ -167,6 +186,12 @@ const SignUpPage = () => {
 					</div>
 				</div>
 			</div>
+
+			{/* right side */}
+			<AuthImagePattern
+				title="Join our communiy"
+				subtitle="Connect with friends, share moments, and stay in touch with you"
+			/>
 		</div>
 	);
 };
